39c0a826b10de8e6e3dc405d070af92042b39459
Nick Matvyeyev <mnasoft@gmail.com>
2019-12-28 13:47:04 +0200

    Sat Dec 28 11:47:04 UTC 2019

--- mnas-logical.lisp
+++ mnas-logical.lisp
@@ -2,25 +2,27 @@
 
 (in-package #:mnas-logical)
 
-(defun and-func(lst)
-  "Аналог макроса and, выполняемого поэлементно для элементов списка;
-Пример использования:
-;;;; (and-func (mapcar #'(lambda(el) (null el)) '(nil nil (1))))
-;;;; => NIL
-;;;; (apply #'and (mapcar #'(lambda(el) (null el)) '(nil nil (1))))
-;;;; => ERROR: Undefined function: AND
+(export 'and-func)
+(defun and-func (lst)
+  "@b(Описание:) and-func аналог макроса and, выполняемого поэлементно для элементов списка;
+@b(Пример использования:)
+@begin[lang=lisp](code)
+ (and-func (mapcar #'(lambda(el) (null el)) '(nil nil (1)))) => NIL
+ (apply #'and (mapcar #'(lambda(el) (null el)) '(nil nil (1)))) => ERROR: Undefined function: AND
+@end(code)
 "
   (let ((rez t))
     (mapcar #'(lambda (el) (setf rez (and rez (not (null el))))) lst)
     rez))
 
+(export 'or-func)
 (defun or-func(lst)
-  "Аналог макроса or, выполняемого поэлементно для элементов списка;
-Пример использования:
-;;;; (or-func (mapcar #'(lambda(el) (not(null el))) '(nil nil (1))))
-;;;; => T
-;;;; (apply #'or (mapcar #'(lambda(el) (not(null el))) '(nil nil (1))))
-;;;; => ERROR: Undefined function: OR
+  "@b(Описание:) аналог макроса or, выполняемого поэлементно для элементов списка;
+@b(Пример использования:)
+@begin[lang=lisp](code)
+ (or-func (mapcar #'(lambda(el) (not(null el))) '(nil nil (1))))    => T
+ (apply #'or (mapcar #'(lambda(el) (not(null el))) '(nil nil (1)))) => ERROR: Undefined function: OR
+@end(code)
 "
   (let ((rez nil))
     (mapcar #'(lambda (el) (setf rez (or rez (not (null el))))) lst)
--- package.lisp
+++ package.lisp
@@ -1,9 +1,6 @@
 ;;;; package.lisp
 
 (defpackage #:mnas-logical
-  (:use #:cl)
-  (:export and-func
-	   or-func))
-
+  (:use #:cl))
 
 ;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
