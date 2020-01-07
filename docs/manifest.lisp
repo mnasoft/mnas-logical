c43a8e0c9b94196a830e67952b5d226a5840054a
Nick Matvyeyev <mnasoft@gmail.com>
2020-01-07 22:37:58 +0200

    Вт янв  7 22:37:58 EET 2020

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:mnas-logical)
+ :documents ((:title "Mnas-Logical"
+              :authors ("Nick Matvyeyev")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
