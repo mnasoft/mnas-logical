495bbf3435e6c345cf8965215cc898c8c4690e54
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-16 10:01:07 +0300

    16 авг 2021 г. 10:01:06

--- docs/manifest.lisp
+++
@@ -1,9 +0,0 @@
-(:docstring-markup-format :scriba
- :systems (:mnas-logical)
- :documents ((:title "Mnas-Logical"
-              :authors ("Nick Matvyeyev")
-              :output-format (:type :multi-html
-                              :template :minima)
-              :sources ("mnas-logical.scr"
-			"mnas-logical-graph.scr"
-			))))
